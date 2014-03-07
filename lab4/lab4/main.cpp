ef5e9df47a0d87785d19f06eb2860c5f27f6b9de
Paul Simpson <paulsimpson@d-10-156-22-70.dhcp4.washington.edu>
2014-03-06 16:06:38 -0800

    adding main

---
+++ lab4/lab4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  lab4
+//
+//  Created by Paul Simpson on 3/3/14.
+//  Copyright (c) 2014 Paul Simpson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
