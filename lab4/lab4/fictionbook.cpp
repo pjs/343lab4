aefd5a6a688debc8646a2b6cc83aa121b0d80ab2
Paul Simpson <paulsimpson@Pauls-MacBook-Pro.local>
2014-03-07 19:35:53 -0800

    lowercase...

---
+++ lab4/lab4/fictionbook.cpp
@@ -0,0 +1,15 @@
+//
+// fictionBook.cpp
+//
+// Paul Simpson
+// Trevor Olson
+//
+
+#include "fictionBook.h"
+
+FictionBook::FictionBook(string author, string title, int year) : 
+	Book(author, title, year){}
+
+int FictionBook::hash() const{
+	return 'f' - 'a';
+}
---
+++ lab4/lab4/youthbook.cpp
@@ -0,0 +1,15 @@
+//
+// youthBook.cpp
+//
+// Paul Simpson
+// Trevor Olson
+//
+
+#include "youthBook.h"
+
+YouthBook::YouthBook(string author, string title, int year)
+	:Book(author, title, year){}
+
+int YouthBook::hash() const{
+	return 'y' - 'a';
+}
